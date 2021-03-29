807d0b5f5c0ffa8ed0891d34b96a72cd621f8631
ADD415 <ADAVID212@caledonian.ac.uk>
2021-03-29 16:19:32 +0100

    Rome

---
+++ Assets/Scripts/sceenTransitions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceenTransitions : MonoBehaviour
+{
+    public UIManager manager;
+
+    public void Room()
+    {
+        manager.livesTransfer();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
